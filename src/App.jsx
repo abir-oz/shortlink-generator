46c60d0334f147f892123dfd9b2c1048efa648b5
abir-oz <jahirul15-8163@diu.edu.bd>
2022-10-19 02:42:47 +0600

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import { motion, useScroll } from "framer-motion";
-import styled from "styled-components";
 import Boost from "./sections/Boost";
 import Footer from "./sections/Footer";
 import Header from "./sections/Header";
 import HomeHero from "./sections/HomeHero";
 import MainSection from "./sections/MainSection";
-import { colors, GlobalStyle } from "./components/styled/globalStyle";
-
-const ProgressBar = styled(motion.div)`
-  height: 2px;
-  background: ${colors.primary.cyan};
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  transform-origin: 0%;
-  z-index: 999999999999999;
-`;
+import { GlobalStyle } from "./components/styled/globalStyle";
 
 function App() {
-  const { scrollYProgress } = useScroll();
-
   return (
     <>
       <GlobalStyle />
-      <ProgressBar style={{ scaleX: scrollYProgress }} />
-      <main>
+      <main role="main">
         <Header />
         <HomeHero />
         <MainSection />
